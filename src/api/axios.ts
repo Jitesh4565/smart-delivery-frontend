91da4d7502d91b8680f6a651304ee3c1c88d50b4
Jitesh4565 <jitujadhav28@gmail.com>
2025-05-13 21:26:23 +0530

    Api

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance=axios.create({
-    baseURL:"http://localhost:5000/api",
+    baseURL: process.env.REACT_APP_API_URL,
 });
+
+console.log('API URL:', process.env.REACT_APP_API_URL);
 
 export default instance;
